934bca7b8b4fe65b5e1fac8d78718fcba14b4e1f
Grace <grace.anderson.au@gmail.com>
2022-06-25 07:56:21 +1000

    draft thought model completed

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,30 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      //use a JavaScript date library of your choice or the native JavaScript Date object to format timestamp
+      createdAt: Date.now(),
+      // Use a getter method to format the timestamp on query
+    },
+    username: {
+      // The user that created this thought)
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+    //reactions are like replies
+    // Array of nested documents created with the reactionSchema
+  }
+  // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+);
 
-})
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,26 +1,27 @@
 const { Schema, model } = require("mongoose");
 
 //Schema to create User model
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    //how to "unique"
-    //how to "Trimmed"
-  },
-  email: {
-    type: String,
-    required: true,
-    //how to "unique"
-    //Must match a valid email address (look into Mongoose's matching validation)
-  },
-  //thoughts - Array of _id values referencing the Thought model
-  thoughts: [thoughtSchema],
-  //friends - Array of _id values referencing the User model (self-reference)
-  friends: [userSchema], //self-reference?
-
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      //"Unique"?
+      //"Trimmed"?
+    },
+    email: {
+      type: String,
+      required: true,
+      //"Unique"?
+      //Must match a valid email address (look into Mongoose's matching validation)
+    },
+    //thoughts - Array of _id values referencing the Thought model
+    thoughts: [thoughtSchema],
+    //friends - Array of _id values referencing the User model (self-reference)
+    friends: [userSchema], //self-reference?
+  }
   //Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-});
+);
 
 const User = model("user", userSchema);
 
