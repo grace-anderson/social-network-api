0d4dc96d0abd84c16dedde791327a7987328ca1e
Grace <grace.anderson.au@gmail.com>
2022-06-25 11:09:18 +1000

    added get single user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,4 +14,29 @@ module.exports = {
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
+
+  //Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Update a single user
+  // db.groceryCollection.updateOne({"item": "banana"}, {$set: { "item" : "apple"}});
+  updateSingleUser(req, res) {
+    User.findOneAndUpdate({ id: req.params.userId }, { new: true })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,14 +1,38 @@
 const router = require('express').Router();
 const {
   getUsers,
-//   getSingleUser,
+  getSingleUser,
   createUser,
+  updateSingleUser,
 } = require('../../controllers/userController');
 
+// GET all users
+// POST a new user:
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
+// GET a single user by its _id
+// with populated thought and friend data
 // /api/users/:userId
-// router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser);
 
 module.exports = router;
+
+// // example data
+// {
+//   "username": "lernantino",
+//   "email": "lernantino@gmail.com"
+// }
+
+// PUT to update a user by its _id
+router.route('/:userId').put(updateSingleUser);
+
+// DELETE to remove user by its _id
+
+// BONUS: Remove a user's associated thoughts when deleted.
+
+// /api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
